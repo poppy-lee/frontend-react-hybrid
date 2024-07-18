d52d5524a195ce24d065a1d5c19bc26a62c77eb4
Poppy Lee <ppoblee@gmail.com>
2024-07-18 13:52:58 +0900

    install @expo/next-adapter into apps/web

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,18 +1,11 @@
-module.exports = {
+const { withExpo } = require("@expo/next-adapter");
+
+/**
+ * @type {import("next").NextConfig}
+ */
+const nextConfig = withExpo({
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      // Transform all direct `react-native` imports to `react-native-web`
-      "react-native$": "react-native-web",
-    };
-    config.resolve.extensions = [
-      ".web.js",
-      ".web.jsx",
-      ".web.ts",
-      ".web.tsx",
-      ...config.resolve.extensions,
-    ];
-    return config;
-  },
-};
+  transpilePackages: ["react-native", "expo"],
+});
+
+module.exports = nextConfig;
