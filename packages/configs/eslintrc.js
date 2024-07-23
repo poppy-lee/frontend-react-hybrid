504b711ba29f86ffb500a71b77ca1328981eef5e
Poppy Lee <ppoblee@gmail.com>
2024-07-23 18:08:02 +0900

    install eslint-config-prettier into @repo/configs

--- packages/configs/eslintrc.js
+++ packages/configs/eslintrc.js
@@ -1,8 +1,13 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["plugin:react/recommended", "plugin:react-hooks/recommended"],
+  extends: [
+    "plugin:prettier/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
   parser: "@typescript-eslint/parser",
   rules: {
+    "prettier/prettier": "warn",
     "react/jsx-uses-react": "off",
     "react/jsx-uses-vars": "off",
     "react/react-in-jsx-scope": "off",
